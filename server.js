760e0297b9aef450b1555ac7279db710a779ff53
harshrastogi15 <hrastogi2002@gmail.com>
2022-05-08 22:16:08 +0530

    React_App_setup

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express= require('express');
 const connect = require('./db');
 require('dotenv').config();
 connect();
-const port= process.env.PORT || 3000;
+const port= process.env.PORT || 3001;
 const app=express();
 
 app.use(bodyParser.urlencoded({extended:true}));
