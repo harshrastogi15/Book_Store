b7642ff3b3017a0488c59c1b681e1adc4e960265
harshrastogi15 <hrastogi2002@gmail.com>
2022-05-12 23:06:01 +0530

    Deploy app

--- server.js
+++ server.js
@@ -1,19 +1,22 @@
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt');
-const express= require('express');
+const express = require('express');
 const path = require('path');
 const connect = require('./db');
 require('dotenv').config();
 connect();
-const port= process.env.PORT || 3001;
-const app=express();
+const port = process.env.PORT || 3001;
+const app = express();
 app.use(express.static(path.resolve(__dirname, '../client/build')));
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
-app.use('/api/auth',require('./routes/auth'));
-app.use('/api/books',require('./routes/booksdetail'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/books', require('./routes/booksdetail'));
 
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`server listen at port ${port}`);
 });
