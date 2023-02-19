9de8ce12b449d68d3105734a7de6e604bc89e0a3
harshrastogi15 <hrastogi2002@gmail.com>
2023-02-19 18:28:48 +0530

    update

--- middleware/mail.js
+++ middleware/mail.js
@@ -6,6 +6,8 @@ const linksOfWebsite = process.env.Website_Link;
 
 const transporter = nodemailer.createTransport({
     service: 'gmail',
+    port: 465,
+    secure: true,
     auth: {
         user: process.env.EMAIL,
         pass: process.env.EMAIL_PASSWORD
